a7f324a6359a606418423153e714b5341f062208
unknown <matheusdorea33@gmail.com>
2026-04-23 13:53:55 -0300

    feat: adicionando personagem principal e movimentação com transformação de matrizes

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-import numpy as np
-from math import cos, sin, radians
-
+from config import ALTURA, LARGURA
 from coresEnum import Cores
 from estadosEnum import Estados
+from nave import Nave
 
 # ============================================================
 # CONFIGURAÇÕES GLOBAIS
@@ -13,14 +12,23 @@ from estadosEnum import Estados
 pygame.init()
 
 # Definindo tamanho da tela
-LARGURA, ALTURA = 900, 700
 TELA = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("Space Defender")
 
 clock = pygame.time.Clock()
 FPS = 60
 
+#Criação de objetos
+def criar_jogo():
+    nave = Nave(LARGURA // 2, ALTURA // 2)
+
+    return nave
+
+# GameLoop
 def main():
+    # Definindo Objetos
+    nave = criar_jogo()
+
     # Definindo estado inicial do jogo
     estado = Estados.ESTADO_JOGANDO
 
@@ -33,11 +41,25 @@ def main():
                 pygame.quit()
                 exit()
 
+        # ---------- INPUT CONTÍNUO ----------
+        if estado == Estados.ESTADO_JOGANDO:
+            teclas = pygame.key.get_pressed()
+            if teclas[K_w] or teclas[K_UP]:
+                nave.mover_frente()
+            if teclas[K_s] or teclas[K_DOWN]:
+                nave.mover_tras()
+            if teclas[K_a] or teclas[K_LEFT]:
+                nave.girar(-1)
+            if teclas[K_d] or teclas[K_RIGHT]:
+                nave.girar(+1)
+
+        # --------- DESENHANDO NA TELA ----------
         if estado == Estados.ESTADO_JOGANDO:
             TELA.fill(Cores.PRETO)
 
+            nave.desenhar(TELA)
 
-        pygame.display.flip()
+            pygame.display.flip()
 
 if __name__ == "__main__":
     main()
